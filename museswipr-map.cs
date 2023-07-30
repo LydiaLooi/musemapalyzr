c548d27a7aa2bc25690c598c6601626df39eaa15
Lydia Looi <itslydia.looi@gmail.com>
2023-07-30 13:53:13 +1200

    Got rid of unused class and method

--- museswipr-map.cs
+++ museswipr-map.cs
@@ -6,12 +6,6 @@ using Newtonsoft.Json.Linq;
 namespace MuseMapalyzr
 {
 
-    public class Track
-    {
-        public string mEventID { get; set; }
-        public List<NoteTimeData> mEventList { get; set; }
-    }
-
     public class NoteTimeData
     {
         public int mStartSample { get; set; }
@@ -20,7 +14,7 @@ namespace MuseMapalyzr
 
     public class MuseSwiprMap
     {
-        public string Title { get; set; }
+        public string Title { get; set; } = "";
         public JToken TempoSections { get; set; }
         public JToken Tracks { get; set; }
         public List<Note> Notes { get; set; } = new List<Note>();
@@ -48,54 +42,6 @@ namespace MuseMapalyzr
 
         }
 
-        public void OutputNotes(string filePath) // WHO CARES DON"T DEAL WITH THIS.
-        {
-            Console.WriteLine("Outputting notes");
-            string mapOutput = Constants.mapOutputs;
-
-            List<Note> notes = this.Notes;
-
-            // Find the smallest time distance between notes
-            int smallestTimeDistance = notes.Zip(notes.Skip(1), (note1, note2) => note2.SampleTime - note1.SampleTime).Min();
-
-            // Sort the notes by descending time order
-            List<Note> sortedNotes = notes.OrderByDescending(note => note.SampleTime).ToList();
-
-            // Get the range of time values to iterate over
-            int startTime = sortedNotes.Last().SampleTime;
-            int endTime = sortedNotes.First().SampleTime + smallestTimeDistance;
-            List<int> timeRange = Enumerable.Range(startTime, endTime - startTime).ToList();
-
-            Console.WriteLine($"Smallest time distance: {smallestTimeDistance}");
-
-            // Write the output to the file
-            foreach (Note note in sortedNotes) {
-                Console.WriteLine($"{note.SampleTime} {note.Lane}");
-            }
-            // using (StreamWriter file = new StreamWriter(Path.Combine(mapOutput, filePath)))
-            // {
-            //     foreach (int time in timeRange.AsEnumerable().Reverse())
-            //     {
-            //         foreach (Note note in sortedNotes)
-            //         {
-            //             // Console.WriteLine($"Note {note.SampleTime} Lane {note.Lane} Event ID {note.Lane} Sample time: {note.SampleTime}");
-            //             if (note.SampleTime <= time && time < note.SampleTime + smallestTimeDistance)
-            //             {
-            //                 if (note.Lane == 0)
-            //                 {
-            //                     file.WriteLine($"{(double)note.SampleTime / this.SampleRate:F2}| [0]");
-            //                 }
-            //                 else if (note.Lane == 1)
-            //                 {
-            //                     file.WriteLine($"{(double)note.SampleTime / this.SampleRate:F2}|      [1]");
-            //                 }
-            //                 break;
-            //             }
-            //         }
-            //         file.WriteLine($"{(double)time / this.SampleRate:F2}|");
-            //     }
-            // }
-        }
 
         public void ParseNotes()
         {
